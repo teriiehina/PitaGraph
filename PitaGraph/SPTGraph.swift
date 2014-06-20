ce8401aef2bcb2aeffe105e92fe1a9064c8dc501
Peter MEUEL <peter@teriiehina.net>
2014-06-20 22:41:50 +0200

    Add basic objects

---
+++ PitaGraph/SPTGraph.swift
@@ -0,0 +1,13 @@
+//
+//  SPTGraph.swift
+//  PitaGraph
+//
+//  Created by teriiehina on 20/06/2014.
+//  Copyright (c) 2014 teriiehina. All rights reserved.
+//
+
+import UIKit
+
+class SPTGraph: NSObject {
+   
+}
---
+++ PitaGraph/SPTVertice.swift
@@ -0,0 +1,13 @@
+//
+//  SPTVertice.swift
+//  PitaGraph
+//
+//  Created by teriiehina on 20/06/2014.
+//  Copyright (c) 2014 teriiehina. All rights reserved.
+//
+
+import UIKit
+
+class SPTVertice: NSObject {
+   
+}
