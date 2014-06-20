e6430be83b4a903c81d90ce10271dfb3bc8ac655
Peter MEUEL <peter@teriiehina.net>
2014-06-20 22:39:53 +0200

    Initial Commit

---
+++ PitaGraph/PitaGraph.h
@@ -0,0 +1,19 @@
+//
+//  PitaGraph.h
+//  PitaGraph
+//
+//  Created by teriiehina on 20/06/2014.
+//  Copyright (c) 2014 teriiehina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PitaGraph.
+FOUNDATION_EXPORT double PitaGraphVersionNumber;
+
+//! Project version string for PitaGraph.
+FOUNDATION_EXPORT const unsigned char PitaGraphVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PitaGraph/PublicHeader.h>
+
+
