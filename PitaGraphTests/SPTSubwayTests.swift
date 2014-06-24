751bb42b5445dfd0b4f70cc4223fa8096190dac9
Peter MEUEL <peter@teriiehina.net>
2014-06-25 00:28:15 +0200

    Add a test and optimize setup.
    The loading of the subway datas takes 14 seconds !!!!
    One can say it's not optimized. Thus we added a global variable
    to load these datas only once.
    
    Add a test to ensure that no station has less than one neighbor
    nor more than three (because that's how the Paris' subway is made).
    This will be false when we will add transfert by walk.

--- PitaGraphTests/SPTSubwayTests.swift
+++ PitaGraphTests/SPTSubwayTests.swift
@@ -11,9 +11,10 @@ import XCTest
 
 import PitaGraph
 
+var globalGraph: SPTGraph?
+
 class SPTSubwayTests: XCTestCase
 {
-
   var graph: SPTGraph?
   
   override func setUp()
@@ -26,42 +27,53 @@ class SPTSubwayTests: XCTestCase
     let dict: NSDictionary? = NSDictionary(contentsOfFile:path)
     let datas = dict!
     
-    self.graph     = SPTGraph()
-    
-    let stops     = datas["stops"]    as Array<Dictionary<String , String>>
-    let segments  = datas["segments"] as Array<Dictionary<String , String>>
-    
-    for stop in stops
+    if !globalGraph
     {
-      self.graph!.addVertice(SPTVertice(name: stop["name"]! , identifier: stop["stop_id"]!))
-    }
-    
-    for segment in segments
-    {
-      let start = self.graph!.getVertice(segment["start"]!)
-      let end   = self.graph!.getVertice(segment["stop"]!)
+      globalGraph   = SPTGraph()
       
-      self.graph!.addEdge(start!, y: end!)
+      let stops     = datas["stops"]    as Array<Dictionary<String , String>>
+      let segments  = datas["segments"] as Array<Dictionary<String , String>>
+      
+      for stop in stops
+      {
+        globalGraph!.addVertice(SPTVertice(name: stop["name"]! , identifier: stop["stop_id"]!))
+      }
+      
+      for segment in segments
+      {
+        let start = globalGraph!.getVertice(segment["start"]!)
+        let end   = globalGraph!.getVertice(segment["stop"]!)
+        
+        globalGraph!.addEdge(start!, y: end!)
+      }
     }
+    
+    self.graph = globalGraph
+
   }
   
   func testImportSubwayDatas()
   {
-    XCTAssertTrue(self.graph, "We should have a non-nil graph")
-
     XCTAssertEqual(self.graph!.order(), 381, "We should have 381 stops")
     XCTAssertEqual(self.graph!.size() , 722, "We should have 722 segments")
   }
   
   func testLaFourche()
   {
-    XCTAssertTrue(self.graph, "We should have a non-nil graph")
-    
     let laFourche = self.graph!.getVertice("355")
     
     XCTAssertTrue(laFourche, "'La Fourche' should exist")
     XCTAssertEqual(laFourche!.name, "La Fourche" , "We should have retrieved 'La Fourche'")
     
     XCTAssertEqual(self.graph!.neighbors(laFourche!).count, 3, "'La Fourche' should have three neighbors")
+  }
+  
+  func testMinMax()
+  {
+    for vertice in self.graph!.vertices.values
+    {
+      XCTAssertTrue(self.graph!.neighbors(vertice).count > 0, "\(vertice.name) should have at least 1 neigbor")
+      XCTAssertTrue(self.graph!.neighbors(vertice).count < 4, "\(vertice.name) should have at most 3 neigbor")
+    }
   }
 }
